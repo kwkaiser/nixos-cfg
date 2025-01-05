222f1c527929287e3c4c53e21df5fcf037d616cf
Karl Kaiser <karl@kwkaiser.io>
2025-01-05 18:14:49 -0500

    Test zfs config

--- hosts/mainarray-vm/disks.nix
+++ hosts/mainarray-vm/disks.nix
@@ -27,6 +27,61 @@
           };
         };
       };
+
+      a = {
+        type = "disk";
+        device = "/dev/vdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+
+      b = {
+        type = "disk";
+        device = "/dev/vdc";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+
+      zpool = {
+        zroot = {
+          type = "zpool";
+          mode = "mirror";
+          # Workaround: cannot import 'zroot': I/O error in disko tests
+          options.cachefile = "none";
+          rootFsOptions = {
+            compression = "zstd";
+            "com.sun:auto-snapshot" = "false";
+          };
+
+          datasets = {
+            zfs_fs = {
+              type = "zfs_fs";
+              mountpoint = "/bulk";
+              options."com.sun:auto-snapshot" = "true";
+            };
+          };
+        };
+      };
     };
   };
 }
