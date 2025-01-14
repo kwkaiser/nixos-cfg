bc8b5e8728e6d4d6ca74dec899cc79ca51328751
Karl Kaiser <karl@kwkaiser.io>
2025-01-13 21:59:58 -0500

    conditional arg

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,9 +1,11 @@
 { config, pkgs, lib, ... }: {
-  # options = {
-  #   bingus.shell.zsh = lib.mkEnableOption "should use zsh as home shell";
-  # };
+  options = {
+    bingus.shell.zsh.enable = lib.mkEnableOption "should use zsh as home shell";
+  };
 
-  home.packages = [ pkgs.zsh ];
-  programs.zsh.enable = true;
+  config = lib.mkIf config.bingus.shell.zsh.enable {
+    home.packages = [ pkgs.zsh ];
+    programs.zsh.enable = true;
+  };
 
 }
