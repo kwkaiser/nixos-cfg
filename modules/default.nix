9d848c6ee88d5c1be2e85d394d52e647ff67d51f
kwkaiser <karl@kwkaiser.io>
2025-01-19 17:15:19 -0500

    dir

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,9 @@
 { pkgs, config, lib, inputs, archi, commonArgs, ... }: {
+  options = {
+    bingus.username = lib.mkEnableOption "Username for the primary user";
+    bingus.homeDir = lib.mkEnableOption "Home directory for the primary user";
+  };
+
   # HM only modules
   home-manager.users.${commonArgs.primaryUser} = { imports = [ ./hm.nix ]; };
 
