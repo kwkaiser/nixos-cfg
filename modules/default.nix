bdc49f83bea5e65232db72a280dc85b52765db74
kwkaiser <karl@kwkaiser.io>
2025-01-19 17:01:07 -0500

    Set state version value

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,8 @@
-{ pkgs, config, lib, inputs, archi, ... }: {
+{ pkgs, config, lib, inputs, archi, commonArgs, ... }: {
+  # HM only modules
+  home-manager.users.${commonArgs.primaryUser} = { imports = [ ./hm.nix ]; };
+
+  # Other modules
   imports = [ ./shell ];
 
 }
