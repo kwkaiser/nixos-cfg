2170d100cfc81b07e98d08bc7e7b9a8fcd7c1823
kwkaiser <karl@kwkaiser.io>
2025-01-19 16:12:44 -0500

    Define users via darwin first

--- modules/mix/default.nix
+++ modules/mix/default.nix
@@ -1,5 +1,12 @@
 { pkgs, lib, inputs, archi, ... }: {
 
+  users.users.kwkaiser = {
+    isNormalUser = true;
+    home = "/Users/kwkaiser";
+    description = "Primary user";
+    extraGroups = [ "wheel" "staff" ];
+  };
+
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager = {
