619df878f10a0e52e9f61944e3191ba713996041
kwkaiser <karl@kwkaiser.io>
2025-01-20 10:34:17 -0500

    Basic aerospace config

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,10 +1,1 @@
-{ pkgs, config, lib, inputs, ... }: {
-  imports = [ ];
-
-  # HM only modules
-  home-manager.extraSpecialArgs = {
-    inherit inputs;
-    bconfig = config;
-  };
-  home-manager.users.${config.mine.username} = { imports = [ ./hm.nix ]; };
-}
+{ pkgs, config, lib, inputs, ... }: { imports = [ ./desktop ]; }
--- modules/darwin/desktop/aero.nix
+++ modules/darwin/desktop/aero.nix
@@ -6,18 +6,36 @@
 
   config = lib.mkIf config.mine.desktop.tiling.enable {
     services.aerospace.enable = true;
+    system.defaults.dock.autohide = true;
     services.aerospace.settings = {
-      gaps = {
-        outer.left = 8;
-        outer.bottom = 8;
-        outer.top = 8;
-        outer.right = 8;
-      };
       mode.main.binding = {
-        alt-h = "focus left";
-        alt-j = "focus down";
-        alt-k = "focus up";
-        alt-l = "focus right";
+        cmd-h = "focus left";
+        cmd-j = "focus down";
+        cmd-k = "focus up";
+        cmd-l = "focus right";
+        cmd-shift-h = "move left";
+        cmd-shift-j = "move down";
+        cmd-shift-k = "move up";
+        cmd-shift-l = "move right";
+        cmd-1 = "workspace 1";
+        cmd-2 = "workspace 2";
+        cmd-3 = "workspace 3";
+        cmd-4 = "workspace 4";
+        cmd-5 = "workspace 5";
+        cmd-6 = "workspace 6";
+        cmd-7 = "workspace 7";
+        cmd-8 = "workspace 8";
+        cmd-9 = "workspace 9";
+        cmd-0 = "workspace 10";
+        cmd-shift-1 = "move-node-to-workspace 1";
+        cmd-shift-2 = "move-node-to-workspace 2";
+        cmd-shift-3 = "move-node-to-workspace 3";
+        cmd-shift-4 = "move-node-to-workspace 4";
+        cmd-shift-5 = "move-node-to-workspace 5";
+        cmd-shift-6 = "move-node-to-workspace 6";
+        cmd-shift-7 = "move-node-to-workspace 7";
+        cmd-shift-8 = "move-node-to-workspace 8";
+        cmd-shift-9 = "move-node-to-workspace 9";
       };
     };
   };
