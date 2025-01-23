8efdb64ad332c062d9936a1fd2ed5514e215efb8
Karl Kaiser <karl@kwkaiser.io>
2025-01-23 00:01:35 -0500

    better binds

--- modules/desktop/tiling/nixos/home.nix
+++ modules/desktop/tiling/nixos/home.nix
@@ -18,8 +18,28 @@
     "$mod" = "SUPER";
     "$terminal" = "kitty";
 
-    bind =
-      [ "$mod, Return, exec, $terminal" "$mod, X, killactive" "$mod, M, exit" ];
+    bind = [
+      "$mod, Return, exec, $terminal"
+      "$mod SHIFT, Q, killactive"
+      "$mod SHIFT, E, exit"
+      # Navigation
+      "$mod, h, movefocus, l"
+      "$mod, l, movefocus, r"
+      "$mod, j, movefocus, d"
+      "$mod, k, movefocus, u"
+
+      # Workspaces
+    ] ++ (builtins.concatLists (builtins.genList (i:
+      let _ = i + 1;
+      in [
+        "$mod, ${toString i}, workspace, ${toString i}"
+        "$mod SHIFT, ${toString i}, movetoworkspace, ${toString i}"
+      ]) 9)) ++ [
+        "$mod, 0, workspace, 10"
+      ]
+      # Application launch keybinds
+      ++ [ "$mod SHIFT, D, exec, rofi -show drun" ];
+
   };
 
   # home.file.".config/hypr/hyprland.conf".source =
