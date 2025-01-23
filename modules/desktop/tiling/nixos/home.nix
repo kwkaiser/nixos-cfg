d86de4b1ee37b18bbb037c815602c98b7805e184
Karl Kaiser <karl@kwkaiser.io>
2025-01-22 22:36:05 -0500

    Super basic terminal creation working

--- modules/desktop/tiling/nixos/home.nix
+++ modules/desktop/tiling/nixos/home.nix
@@ -1,19 +1,25 @@
 { pkgs, lib, config, inputs, home, ... }: {
+
+  home.packages = with pkgs; [ kitty gtk3 ];
+
   wayland.windowManager.hyprland.enable = true;
+  services.dunst.enable = true;
+  services.hyprpaper.enable = true;
+  programs.waybar.enable = true;
+  programs.rofi.enable = true;
+
+  home.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+    KITTY_DISABLE_WAYLAND = "0";
+  };
 
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
     "$terminal" = "kitty";
-    disable_logs = false;
-    bind = [ "$mod, ENTER, exec, $terminal" ] ++ (
-      # workspaces
-      # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-      builtins.concatLists (builtins.genList (i:
-        let ws = i + 1;
-        in [
-          "$mod, code:1${toString i}, workspace, ${toString ws}"
-          "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-        ]) 9));
+
+    bind =
+      [ "$mod, Return, exec, $terminal" "$mod, X, killactive" "$mod, M, exit" ];
   };
 
   # home.file.".config/hypr/hyprland.conf".source =
