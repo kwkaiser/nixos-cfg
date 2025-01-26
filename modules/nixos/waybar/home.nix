3e0645d21b166b75c309933ade74bd87f196d162
Karl Kaiser <karl@kwkaiser.io>
2025-01-26 00:04:35 -0500

    Prefix with #

--- modules/nixos/waybar/home.nix
+++ modules/nixos/waybar/home.nix
@@ -36,5 +36,70 @@
         };
       };
     };
+
+    style = ''
+
+      * {
+        font-size: 11px;
+      }
+
+      window#waybar {
+        background: transparent;
+      }
+
+      /* WORKSPACE */
+
+      #workspaces button {
+        background: #${config.colorScheme.palette.base08};
+        color: #${config.colorScheme.palette.base0F};
+        min-width: 8px;
+        padding-left: 16px;
+        padding-right: 16px;
+        padding-top: 6px;
+        padding-bottom: 6px;
+        margin-top: 8px;
+        margin-left: 4px;
+        margin-right: 4px;
+        border: 1px solid #${config.colorScheme.palette.base04};
+        border-radius: 7px;
+      }
+
+      #workspaces button.focused {
+        color: #${config.colorScheme.palette.base07};
+        background: #${config.colorScheme.palette.base00};
+        border: 1px solid #${config.colorScheme.palette.base02};
+      }
+
+      #workspaces button.urgent {
+        color: #${config.colorScheme.palette.base07};
+        background: #${config.colorScheme.palette.base00};
+        border: 1px solid #${config.colorScheme.palette.base01};
+      }
+
+      /* SYS STATS */
+
+      #cpu,
+      #memory,
+      #clock,
+      #disk,
+      #network {
+        color: #${config.colorScheme.palette.base0E};
+        background: #${config.colorScheme.palette.base00};
+        border: 1px solid #${config.colorScheme.palette.base02};
+        border-radius: 7px;
+        font-size: 14;
+        min-width: 60px;
+        padding-left: 32px;
+        padding-right: 32px;
+        margin-bottom: 8px;
+        margin-left: 8px;
+        margin-right: 8px;
+      }
+
+      #network {
+        padding-left: 6px;
+        padding-right: 6px;
+      }
+    '';
   };
 }
