9478c91e94d7c32640f17cb9f48f9fd52913c2fe
Karl Kaiser <karl@kwkaiser.io>
2025-01-26 00:04:35 -0500

    Prefix with #

--- modules/common/kitty/home.nix
+++ modules/common/kitty/home.nix
@@ -6,6 +6,10 @@
         foreground = "#${config.colorScheme.palette.base05}";
         background = "#${config.colorScheme.palette.base00}";
       };
+      extraConfig = ''
+        confirm_os_window_close 0 
+      '';
     };
+
   };
 }
