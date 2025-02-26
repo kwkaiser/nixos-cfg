9c50f66337dcb242e8a19eae94096054dad94caf
kwkaiser <karl@kwkaiser.io>
2025-02-26 11:24:03 -0500

    Add opts around node stuff

--- modules/common/zsh/home.nix
+++ modules/common/zsh/home.nix
@@ -9,6 +9,10 @@
     };
     initExtra = ''
       export NVM_DIR="$HOME/.nvm"
+      export PATH="/opt/homebrew/opt/jpeg/bin:$PATH"
+      export LDFLAGS="-L/opt/homebrew/opt/jpeg/lib"
+      export CPPFLAGS="-I/opt/homebrew/opt/jpeg/include"
+      export PKG_CONFIG_PATH="/opt/homebrew/opt/jpeg/lib/pkgconfig" 
       [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
       [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
     '';
