968b52422e9376833665b1a4fd1157adc08d01a0
kwkaiser <karl@kwkaiser.io>
2025-03-08 18:53:09 -0600

    Conditionally attach session vars to init for zsh

--- modules/common/zsh/home.nix
+++ modules/common/zsh/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, bconfig, ... }: {
   programs.direnv.enable = true;
   programs.zsh = {
     enable = true;
@@ -7,14 +7,5 @@
       enable = true;
       theme = "robbyrussell";
     };
-    initExtra = ''
-      export NVM_DIR="$HOME/.nvm"
-      export PATH="/opt/homebrew/opt/jpeg/bin:$PATH"
-      export LDFLAGS="-L/opt/homebrew/opt/jpeg/lib"
-      export CPPFLAGS="-I/opt/homebrew/opt/jpeg/include"
-      export PKG_CONFIG_PATH="/opt/homebrew/opt/jpeg/lib/pkgconfig" 
-      [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-      [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
-    '';
   };
 }
