84a1aa246674c3af8587ab78ea7ef7178ff554fd
kwkaiser <karl@kwkaiser.io>
2025-04-03 17:47:45 -0400

    Conditional kde logic

--- modules/common/messaging/default.nix
+++ modules/common/messaging/default.nix
@@ -1,11 +1,25 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, isDarwin, ... }: {
 
   options = {
-    mine.messaging.enable = lib.mkEnableOption "Enables messaging applications (Slack, Signal, Caprine, Discord)";
+    mine.messaging.enable = lib.mkEnableOption
+      "Enables messaging applications (Slack, Signal, Caprine, Discord)";
   };
 
-  config = lib.mkIf config.mine.messaging.enable {
-    # Home manager config
-    home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
-  };
-} 
+  config = lib.mkIf config.mine.messaging.enable (lib.mkMerge [
+    {
+      home-manager.users.${config.mine.username} = {
+        imports = [ ./home.nix ];
+      };
+    }
+    # For KDE connect
+    (lib.mkIf (!isDarwin) {
+      networking.firewall = rec {
+        allowedTCPPortRanges = [{
+          from = 1714;
+          to = 1764;
+        }];
+        allowedUDPPortRanges = allowedTCPPortRanges;
+      };
+    })
+  ]);
+}
