174f096b0a0210db5f0fb7851858cd762568f0ca
Karl Kaiser <karl@kwkaiser.io>
2025-04-03 19:42:54 -0400

    Fix evaluation issues on nixos

--- modules/common/cursor/default.nix
+++ modules/common/cursor/default.nix
@@ -4,13 +4,11 @@
     mine.cursor.enable = lib.mkEnableOption "Installs & configures cursor";
   };
 
-  config = lib.mkIf config.mine.cursor.enable (lib.mkMerge [
-    {
-      home-manager.users.${config.mine.username} = {
-        imports = [ ./home.nix ];
-      };
-    }
-
-    (lib.mkIf isDarwin { homebrew.casks = [ "cursor" ]; })
-  ]);
+  config = lib.mkIf config.mine.cursor.enable {
+    home-manager.users.${config.mine.username} = {
+      imports = [ ./home.nix ];
+    };
+  } // (if isDarwin then {
+    homebrew.casks = [ "cursor" ];
+  } else { });
 }
--- modules/common/steam/default.nix
+++ modules/common/steam/default.nix
@@ -3,14 +3,12 @@
     mine.steam.enable = lib.mkEnableOption "Enables steam gaming platform";
   };
 
-  config = lib.mkIf config.mine.steam.enable (lib.mkMerge [
-    {
-      home-manager.users.${config.mine.username} = {
-        imports = [ ./home.nix ];
-      };
-    }
-
-    (lib.mkIf isDarwin { homebrew.casks = [ "steam" ]; })
-  ]);
+  config = lib.mkIf config.mine.steam.enable {
+    home-manager.users.${config.mine.username} = {
+      imports = [ ./home.nix ];
+    };
+  } // (if isDarwin then {
+    homebrew.casks = [ "steam" ];
+  } else { });
 }
 
