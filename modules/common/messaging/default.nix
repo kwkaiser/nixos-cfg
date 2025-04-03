598f4af02ca0d75f6cc2292436fdbbd76589f56f
kwkaiser <karl@kwkaiser.io>
2025-04-03 17:50:48 -0400

    don't alter port ranges (yet)

--- modules/common/messaging/default.nix
+++ modules/common/messaging/default.nix
@@ -5,21 +5,8 @@
       "Enables messaging applications (Slack, Signal, Caprine, Discord)";
   };
 
-  config = lib.mkIf config.mine.messaging.enable (lib.mkMerge [
-    {
-      home-manager.users.${config.mine.username} = {
-        imports = [ ./home.nix ];
-      };
-    }
-    # For KDE connect
-    (lib.mkIf (!isDarwin) {
-      networking.firewall = rec {
-        allowedTCPPortRanges = [{
-          from = 1714;
-          to = 1764;
-        }];
-        allowedUDPPortRanges = allowedTCPPortRanges;
-      };
-    })
-  ]);
+  config = lib.mkIf config.mine.messaging.enable {
+    # Home manager config
+    home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
+  };
 }
