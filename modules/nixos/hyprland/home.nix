d7bc591488ae4eafd07dc37179ff92a3f699b805
Karl Kaiser <karl@kwkaiser.io>
2025-04-09 23:13:53 -0400

    Working hyprlock

--- modules/nixos/hyprland/home.nix
+++ modules/nixos/hyprland/home.nix
@@ -1,5 +1,4 @@
 { pkgs, lib, config, inputs, home, bconfig, ... }: {
-
   wayland.windowManager.hyprland.enable = true;
   services.dunst.enable = true;
   services.hyprpaper.enable = true;
@@ -13,6 +12,7 @@
       "$mod, Return, exec, $terminal"
       "$mod SHIFT, Q, killactive"
       "$mod SHIFT, E, exit"
+      "$mod SHIFT, X, exec, hyprlock"
       # Navigation
       "$mod, h, movefocus, l"
       "$mod, l, movefocus, r"
@@ -37,4 +37,28 @@
 
   };
 
+  programs.hyprlock.enable = true;
+  programs.hyprlock.settings = {
+    general = {
+      disable_loading_bar = true;
+      hide_cursor = true;
+      no_fade_in = false;
+    };
+
+    background = { color = "rgb(24, 25, 38)"; };
+
+    input-field = {
+      size = "200, 50";
+      position = "0, -80";
+      monitor = "";
+      dots_center = true;
+      fade_on_empty = false;
+      font_color = "rgb(202, 211, 245)";
+      inner_color = "rgb(91, 96, 120)";
+      outer_color = "rgb(24, 25, 38)";
+      outline_thickness = 5;
+      placeholder_text = "Unlock:";
+      shadow_passes = 2;
+    };
+  };
 }
