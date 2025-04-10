a22951e9587974444c27988652b46b92268df826
Karl Kaiser <karl@kwkaiser.io>
2025-04-09 22:37:58 -0400

    rm conflicting def

--- hosts/vm/net.nix
+++ hosts/vm/net.nix
@@ -1,7 +1,5 @@
 { config, pkgs, ... }: {
   networking.hostId = "22a4d930";
   networking.hostName = "vm";
-  networking.networkmanager.enable = true;
-  services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "yes";
+  networking.networkmanager.enable = true; 
 }
