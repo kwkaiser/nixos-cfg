234ab7184b54b7456daf02e475ea29c5dbb2682e
kwkaiser <karl@kwkaiser.io>
2025-04-10 00:04:29 -0400

    Make tweaks required to play nice on macbook

--- modules/common/ssh/default.nix
+++ modules/common/ssh/default.nix
@@ -5,32 +5,24 @@
 
   config = lib.mkIf config.mine.ssh.enable {
     # Enable SSH service
-    services.openssh = {
-      enable = true;
-      settings = {
-        PasswordAuthentication = false;
-        PermitRootLogin = "no";
-      };
-    };
+    services.openssh.enable = true;
 
     # Enable SSH agent
     programs.ssh = {
-      startAgent = true;
       extraConfig = ''
         AddKeysToAgent yes
       '';
     };
 
-  } // (if isDarwin then {
-    # On Darwin, we need to use the launchd service instead
-    launchd.user.agents.ssh-agent = {
-      serviceConfig = {
-        ProgramArguments =
-          [ "/usr/bin/ssh-agent" "-D" "-a" "/tmp/ssh-agent.socket" ];
-        RunAtLoad = true;
-        KeepAlive = true;
+  } // (if isDarwin then
+    { }
+  else {
+    programs.ssh = {
+      startAgent = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
       };
     };
-  } else
-    { });
+  });
 }
