a058bd47e97295e9d7773e541a0ce81034ccec27
kwkaiser <karl@kwkaiser.io>
2025-04-14 21:37:15 -0400

    Try creating linux builder backend

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,4 +2,11 @@
 {
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-} // (if isDarwin then { nix.linux-builder.enable = true; } else { })
+} // (if isDarwin then {
+  nix = {
+    linux-builder.enable = true;
+    settings.trusted-users = [ "@admin" ];
+  };
+
+} else
+  { })
