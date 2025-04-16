6ae734c6a13041dd18f6098e287b3544c3f6f5ee
Karl Kaiser <karl@kwkaiser.io>
2025-04-16 17:32:50 -0400

    script tweaks

--- hosts/vm/create.sh
+++ hosts/vm/create.sh
@@ -19,21 +19,22 @@ mkdir -pv data/vm/shared
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     MACHINE_ARGS="-machine q35,accel=kvm"
+    QEMU_EFI_PATH="/usr/share/edk2/x64/OVMF_CODE.4m.fd"
     echo "Running on Linux, using KVM acceleration"
 else
+    QEMU_EFI_PATH="$(nix --experimental-features 'nix-command flakes' eval --raw nixpkgs#qemu)/share/qemu/edk2-x86_64-code.fd"
     MACHINE_ARGS="-machine q35,accel=tcg"
     echo "Running on macOS, using TCG emulation"
 fi
 
 # Get QEMU share directory path
-QEMU_EFI_PATH="$(nix --experimental-features 'nix-command flakes' eval --raw nixpkgs#qemu)/share/qemu/edk2-x86_64-code.fd"
 
 # Run the VM
 qemu-system-x86_64 \
   -m 10G \
   -smp 6 \
   -cpu Haswell \
-  $MACHINE_ARGS \
+  "$MACHINE_ARGS" \
   -drive if=pflash,format=raw,readonly=on,file="$QEMU_EFI_PATH" \
   -drive file=data/vm/dev1.qcow2,format=qcow2,if=none,id=drive1 \
   -device virtio-blk-pci,drive=drive1,serial=dev1 \
