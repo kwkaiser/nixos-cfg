b6bf1352c663a8320c815473c8b28d020aae7de4
kwkaiser <karl@kwkaiser.io>
2025-04-15 21:33:40 -0400

    Working VM on osx!

--- hosts/vm/create.sh
+++ hosts/vm/create.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
 # Create VM directory if it doesn't exist
-mkdir -pv data/vm
+mkdir -pv data/vm-thin
 
 # Create disk images if they don't exist
-if [ ! -f "data/vm/dev1.qcow2" ]; then
-    qemu-img create -f qcow2 data/vm/dev1.qcow2 20G
+if [ ! -f "data/vm-thin/dev1.qcow2" ]; then
+    qemu-img create -f qcow2 data/vm-thin/dev1.qcow2 20G
 fi
-if [ ! -f "data/vm/dev2.qcow2" ]; then
-    qemu-img create -f qcow2 data/vm/dev2.qcow2 5G
+if [ ! -f "data/vm-thin/dev2.qcow2" ]; then
+    qemu-img create -f qcow2 data/vm-thin/dev2.qcow2 5G
 fi
-if [ ! -f "data/vm/dev3.qcow2" ]; then
-    qemu-img create -f qcow2 data/vm/dev3.qcow2 5G
+if [ ! -f "data/vm-thin/dev3.qcow2" ]; then
+    qemu-img create -f qcow2 data/vm-thin/dev3.qcow2 5G
 fi
 
 # Create a shared directory mount point
-mkdir -pv data/vm/shared
+mkdir -pv data/vm-thin/shared
 
-# Mount the host directory
-# mount --bind /home/kwkaiser/desktop/nix data/vm/shared
-
-# Detect OS and set machine/acceleration parameters
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux with KVM
     MACHINE_ARGS="-machine q35,accel=kvm"
     echo "Running on Linux, using KVM acceleration"
 else
-    # macOS with TCG
     MACHINE_ARGS="-machine q35,accel=tcg"
     echo "Running on macOS, using TCG emulation"
 fi
 
+# Get QEMU share directory path
+QEMU_EFI_PATH="$(nix --experimental-features 'nix-command flakes' eval --raw nixpkgs#qemu)/share/qemu/edk2-x86_64-code.fd"
+
 # Run the VM
 qemu-system-x86_64 \
-  -m 4G \
-  -smp 2 \
+  -m 10G \
+  -smp 6 \
   -cpu Haswell \
   $MACHINE_ARGS \
-  -drive file=data/vm/dev1.qcow2,format=qcow2,if=virtio \
-  -drive file=data/vm/dev2.qcow2,format=qcow2,if=virtio \
-  -drive file=data/vm/dev3.qcow2,format=qcow2,if=virtio \
+  -drive if=pflash,format=raw,readonly=on,file="$QEMU_EFI_PATH" \
+  -drive file=data/vm-thin/dev1.qcow2,format=qcow2,if=none,id=drive1 \
+  -device virtio-blk-pci,drive=drive1,serial=dev1 \
+  -drive file=data/vm-thin/dev2.qcow2,format=qcow2,if=none,id=drive2 \
+  -device virtio-blk-pci,drive=drive2,serial=dev2 \
+  -drive file=data/vm-thin/dev3.qcow2,format=qcow2,if=none,id=drive3 \
+  -device virtio-blk-pci,drive=drive3,serial=dev3 \
   -drive file=data/isos/nixos-minimal.iso,format=raw,if=none,id=cdrom \
   -device ide-cd,drive=cdrom \
   -boot d \
@@ -49,6 +50,3 @@ qemu-system-x86_64 \
   -serial mon:stdio \
   -netdev vmnet-shared,id=net0 \
   -device virtio-net-pci,netdev=net0
-
-# Cleanup: unmount the shared directory when done
-# umount data/vm/shared 
