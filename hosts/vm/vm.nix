c8dbf55ca31236e1ea1bd5dcfb1d9354e3712bce
Karl Kaiser <karl@kwkaiser.io>
2025-04-17 01:02:05 -0400

    Simple vm

--- hosts/vm/vm.nix
+++ hosts/vm/vm.nix
@@ -4,20 +4,20 @@
     virtualisation = {
       memorySize = 2048; # Use 2048MiB memory.
       cores = 3;
-      qemu.options = [
-        "-netdev"
-        "user,id=net0,hostfwd=tcp::2222-:22"
-        "-device"
-        "virtio-net-pci,netdev=net0"
-        "-spice"
-        "port=5900,addr=127.0.0.1,disable-ticketing"
-        "-device"
-        "virtio-serial-pci"
-        "-chardev"
-        "spicevmc,id=vdagent,name=vdagent"
-        "-device"
-        "virtserialport,chardev=vdagent,name=com.redhat.spice.0"
-      ];
+      # qemu.options = [
+      #   "-netdev"
+      #   "user,id=net0,hostfwd=tcp::2222-:22"
+      #   "-device"
+      #   "virtio-net-pci,netdev=net0"
+      #   "-spice"
+      #   "port=5900,addr=127.0.0.1,disable-ticketing"
+      #   "-device"
+      #   "virtio-serial-pci"
+      #   "-chardev"
+      #   "spicevmc,id=vdagent,name=vdagent"
+      #   "-device"
+      #   "virtserialport,chardev=vdagent,name=com.redhat.spice.0"
+      # ];
     };
   };
 }
