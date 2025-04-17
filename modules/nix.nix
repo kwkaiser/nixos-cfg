acf288ad7ad7ad4752d7fd8e4197ded2c9bef2a8
karl@kwkaiser.io <karl@kwkaiser.io>
2025-04-17 17:14:21 -0400

    wip getting homebrew to work with multi-user install

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,4 +1,18 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, isDarwin, ... }:
+{
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-}
+} // (if isDarwin then {
+  nix = {
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      systems = [ "x86_64-linux" ];
+    };
+    settings = {
+      trusted-users = [ "@admin" "kwkaiser" ];
+      extra-trusted-users = [ "@admin" "kwkaiser" ];
+    };
+  };
+} else
+  { })
