dc0a7c7b12e82f35c24b6a142f9c643812d79d9f
Karl Kaiser <karl@kwkaiser.io>
2025-04-17 21:46:10 -0400

    Attach disks

--- hosts/vm/vm.nix
+++ hosts/vm/vm.nix
@@ -1,23 +1,10 @@
 { config, pkgs, ... }: {
   virtualisation.vmVariant = {
-    # following configuration is added only when building VM with build-vm
     virtualisation = {
       memorySize = 2048; # Use 2048MiB memory.
       cores = 3;
-      # qemu.options = [
-      #   "-netdev"
-      #   "user,id=net0,hostfwd=tcp::2222-:22"
-      #   "-device"
-      #   "virtio-net-pci,netdev=net0"
-      #   "-spice"
-      #   "port=5900,addr=127.0.0.1,disable-ticketing"
-      #   "-device"
-      #   "virtio-serial-pci"
-      #   "-chardev"
-      #   "spicevmc,id=vdagent,name=vdagent"
-      #   "-device"
-      #   "virtserialport,chardev=vdagent,name=com.redhat.spice.0"
-      # ];
+      diskSize = 5120; # 5GB main disk
+      emptyDiskImages = [ 5120 5120 ]; # Two additional 5GB disks
     };
   };
 }
