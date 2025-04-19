a4a162f3965f2df14bbd9e7809bc454728f31569
Karl Kaiser <karl@kwkaiser.io>
2025-04-19 17:00:13 -0400

    Disable stylix

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -1,5 +1,5 @@
 { pkgs, config, lib, inputs, ... }: {
-  stylix.enable = true;
+  stylix.enable = false;
   stylix.image = ../../assets/backgrounds/cherry.jpg;
   stylix.base16Scheme =
     "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
