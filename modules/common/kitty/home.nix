3a75f3f4b104600324876f9a0c54a82b39147d59
Karl Kaiser <karl@kwkaiser.io>
2025-04-19 17:05:20 -0400

    Restore nix color configs

--- modules/common/kitty/home.nix
+++ modules/common/kitty/home.nix
@@ -5,6 +5,10 @@
       extraConfig = ''
         confirm_os_window_close 0 
       '';
+      settings = {
+        foreground = "#${config.colorScheme.palette.base05}";
+        background = "#${config.colorScheme.palette.base00}";
+      };
     };
 
   };
