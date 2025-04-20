4c7916ec8aa7b3507e27d58dd53a649580f5fe77
Karl Kaiser <karl@kwkaiser.io>
2025-04-19 20:53:30 -0400

    Revert "reset flake?"
    This reverts commit 6b5206360856621705b46cd25c386b6336f8a7d0.

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -2,5 +2,5 @@
   stylix.enable = true;
   stylix.image = ../../assets/backgrounds/cherry.jpg;
   # Other themes available under https://github.com/tinted-theming/base16-schemes
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/spaceduck.yaml";
 }
