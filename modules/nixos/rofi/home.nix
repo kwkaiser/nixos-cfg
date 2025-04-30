0d1b0371684522780437b9509564ad6c06d58c26
Karl Kaiser <karl@kwkaiser.io>
2025-04-30 19:30:26 -0400

    Styling 2

--- modules/nixos/rofi/home.nix
+++ modules/nixos/rofi/home.nix
@@ -18,27 +18,5 @@
       fixed-num-lines = true;
       window-format = "{w}";
     };
-    theme = let inherit (config.lib.formats.rasi) mkLiteral;
-    in {
-      "*" = {
-        background-color = mkLiteral "@base00";
-        text-color = mkLiteral "@base05";
-      };
-
-      "element-text, element-icon" = {
-        background-color = mkLiteral "inherit";
-        text-color = mkLiteral "inherit";
-      };
-
-      "window" = {
-        border-radius = 8;
-        background-color = mkLiteral "@base00";
-      };
-
-      "element selected" = {
-        background-color = mkLiteral "@base02";
-        text-color = mkLiteral "@base06";
-      };
-    };
   };
 }
