5b51a4ec5d18945373590b2f8fdd6996aae40769
Karl Kaiser <karl@kwkaiser.io>
2025-04-30 19:25:39 -0400

    Configure some styles

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -2,5 +2,6 @@
   stylix.enable = true;
   stylix.image = ../../assets/backgrounds/cherry.jpg;
   # Other themes available under https://github.com/tinted-theming/base16-schemes
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/spaceduck.yaml";
+  stylix.base16Scheme =
+    "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 }
