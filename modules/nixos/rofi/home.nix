15d394bf46cf391d0506a5a2c0991b0a64049d01
Karl Kaiser <karl@kwkaiser.io>
2025-04-30 19:45:30 -0400

    rofi config

--- modules/nixos/rofi/home.nix
+++ modules/nixos/rofi/home.nix
@@ -6,17 +6,71 @@
       modi = "drun";
       display-drun = "Run";
       sidebar-mode = false;
-      # Window dimensions
-      width = 50;
-      lines = 10;
-      # Padding and spacing
-      padding = 400;
-      border-radius = 8;
-      # Positioning
-      location = 0;
-      anchor = "center";
-      fixed-num-lines = true;
-      window-format = "{w}";
+    };
+    theme = let inherit (config.lib.formats.rasi) mkLiteral;
+    in {
+      "window" = {
+        transparency = mkLiteral ''"real"'';
+        border-radius = mkLiteral "7px";
+        width = mkLiteral "50%";
+        padding = mkLiteral "5px";
+        border = mkLiteral "1px";
+      };
+
+      "prompt" = {
+        enabled = true;
+        horizontal-align = 0.5;
+        vertical-align = 0.5;
+        border = mkLiteral "2px";
+        border-radius = mkLiteral "7px";
+        padding = mkLiteral "0 1% 0";
+      };
+
+      "entry" = {
+        placeholder = ''"search"'';
+        expand = true;
+        padding = mkLiteral "2%";
+        border = mkLiteral "2px";
+        border-radius = mkLiteral "7px";
+        cursor = mkLiteral "text";
+      };
+
+      "inputbar" = {
+        children = map mkLiteral [ "prompt" "entry" ];
+        expand = false;
+        spacing = mkLiteral "1%";
+      };
+
+      "listview" = {
+        columns = 1;
+        lines = 12;
+        cycle = false;
+        dynamic = true;
+        layout = mkLiteral "vertical";
+      };
+
+      "mainbox" = {
+        children = map mkLiteral [ "inputbar" "listview" ];
+        spacing = mkLiteral "1%";
+        padding = mkLiteral "1% 1% 1% 1%";
+      };
+
+      "element" = {
+        orientation = mkLiteral "vertical";
+        border-radius = mkLiteral "7px";
+        padding = mkLiteral "1% 1%";
+      };
+
+      "element-text" = {
+        expand = true;
+        vertical-align = mkLiteral "0.5";
+      };
+
+      "element selected" = { border-radius = mkLiteral "7px"; };
+
+      "listview, element, element selected, element-icon, element-text" = {
+        cursor = mkLiteral "pointer";
+      };
     };
   };
 }
