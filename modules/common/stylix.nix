e67148dc1413b2cdc9439c6d67f0dd21fd0714d2
Karl Kaiser <karl@kwkaiser.io>
2025-04-30 20:05:26 -0400

    theme exps

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -2,5 +2,6 @@
   stylix.enable = true;
   stylix.image = ../../assets/backgrounds/cherry.jpg;
   # Other themes available under https://github.com/tinted-theming/base16-schemes
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/brewer.yaml";
+  stylix.base16Scheme =
+    "${pkgs.base16-schemes}/share/themes/gruvbox-light-hard.yaml";
 }
