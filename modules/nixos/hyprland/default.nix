de1d7bc9b7a64016cdd72b19cbde3d5642c18542
Karl Kaiser <karl@kwkaiser.io>
2025-04-30 22:10:40 -0400

    Use swww

--- modules/nixos/hyprland/default.nix
+++ modules/nixos/hyprland/default.nix
@@ -2,14 +2,13 @@
 
   options = {
     mine.hyprland.enable = lib.mkEnableOption "Enables hyprland desktop";
-
   };
 
   config = lib.mkIf config.mine.hyprland.enable {
     xdg.portal.enable = true;
     xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
     programs.hyprland.enable = true;
-    security.pam.services.hyprlock = {}; 
+    security.pam.services.hyprlock = { };
 
     # Home manager config
     home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
