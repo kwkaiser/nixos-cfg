e6e0935f66b9ff8f1ea5f441cce47d1f288918ea
Karl Kaiser <karl@kwkaiser.io>
2025-05-24 20:29:02 -0400

    multi-monitor bar behavior

--- modules/nixos/waybar/default.nix
+++ modules/nixos/waybar/default.nix
@@ -3,9 +3,15 @@
   options = {
     mine.waybar.enable = lib.mkEnableOption "Enables waybar as a statusbar";
 
-    mine.waybar.monitor = lib.mkOption {
+    mine.waybar.primaryMonitor = lib.mkOption {
       type = lib.types.str;
-      description = "Primary monitor";
+      description = "Primary monitor for waybar";
+    };
+
+    mine.waybar.secondaryMonitor = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Optional secondary monitor for waybar";
     };
   };
 
