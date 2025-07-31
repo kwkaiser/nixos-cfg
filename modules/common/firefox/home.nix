4ab13f1cd2985dfc3a5288d596c45673adb6458d
karl@kwkaiser.io <karl@kwkaiser.io>
2025-07-30 22:20:56 -0400

    scroll behavior

--- modules/common/firefox/home.nix
+++ modules/common/firefox/home.nix
@@ -3,13 +3,41 @@
     enable = true;
     profiles = {
       kwkaiser = {
-        extensions = with inputs.firefox-addons.packages.${pkgs.system};
-          [
-            ublock-origin
-            keepassxc-browser
-            i-dont-care-about-cookies
-            sponsorblock
-          ];
+        extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+          ublock-origin
+          keepassxc-browser
+          i-dont-care-about-cookies
+          sponsorblock
+        ];
+
+        # Smooth scrolling preferences
+        settings = {
+          # Enable smooth scrolling
+          "general.smoothScroll" = true;
+          "general.smoothScroll.pages" = true;
+          "general.smoothScroll.mouseWheel" = true;
+          "general.smoothScroll.scrollbars" = true;
+
+          # Reduce mouse wheel scroll distance for finer control
+          "mousewheel.min_line_scroll_amount" = 1;
+
+          # Adjust mouse wheel acceleration for smoother feel
+          "mousewheel.acceleration.factor" = 3;
+          "mousewheel.acceleration.start" = 2;
+
+          # Use system scroll settings when possible
+          "mousewheel.system_scroll_override_on_root_content.enabled" = false;
+
+          # Reduce scroll jump distance
+          "toolkit.scrollbox.smoothScroll" = true;
+          "toolkit.scrollbox.verticalScrollDistance" = 3;
+          "toolkit.scrollbox.horizontalScrollDistance" = 5;
+
+          # Additional smooth scrolling tweaks
+          "mousewheel.default.delta_multiplier_x" = 100;
+          "mousewheel.default.delta_multiplier_y" = 100;
+          "mousewheel.default.delta_multiplier_z" = 100;
+        };
       };
     };
   };
