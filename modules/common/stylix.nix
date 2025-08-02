1a2f9ee2e2a4dc4e90db6692a2aefa5257afec82
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-02 14:28:13 -0400

    stylix

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -3,5 +3,5 @@
   stylix.image = ../../assets/backgrounds/cherry.jpg;
   # Other themes available under https://github.com/tinted-theming/base16-schemes
   stylix.base16Scheme =
-    "${pkgs.base16-schemes}/share/themes/gruvbox-light-hard.yaml";
+    "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 }
