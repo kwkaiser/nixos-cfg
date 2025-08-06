cb1057a299724bb5c58b486cbf2e75781ce2eace
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-05 21:05:29 -0400

    rm

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -1,6 +1,6 @@
 { pkgs, config, lib, inputs, ... }: {
   stylix.enable = true;
-  stylix.image = ../../assets/backgrounds/bay-stark.jpg;
+  stylix.image = ../../assets/backgrounds/bay-wharf.jpg;
   # Other themes available under https://github.com/tinted-theming/base16-schemes
   stylix.base16Scheme =
     "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
