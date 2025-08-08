fe12f9f0b382097e4bf06fe4c63d3d7739ea29d2
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-08 15:28:54 -0400

    zsh wip

--- modules/common/zsh/home.nix
+++ modules/common/zsh/home.nix
@@ -17,10 +17,10 @@
             if [ -f ~/.zshrc_homebrew_env ]; then
               source ~/.zshrc_homebrew_env
             fi
-
-      export PATH="$HOME/.encore/bin:$HOME/.local/bin:$PATH"
     '';
 
   };
+
+  home.sessionPath = [ "$HOME/.encore/bin" "$HOME/.local/bin" ];
 
 }
