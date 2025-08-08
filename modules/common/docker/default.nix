0da40569d827d30cfc07beaed3a32bc35d09fea8
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-08 16:21:35 -0400

    Simple docker

--- modules/common/docker/default.nix
+++ modules/common/docker/default.nix
@@ -4,13 +4,8 @@
   };
 
   config = lib.mkIf config.mine.docker.enable {
-    # Enable Docker
-    virtualisation.docker.enable = true;
+    virtualisation.docker = { enable = true; };
 
-    # Add user to docker group
     users.users.${config.mine.username}.extraGroups = [ "docker" ];
-
-    # Home manager configuration
-    home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
   };
 }
