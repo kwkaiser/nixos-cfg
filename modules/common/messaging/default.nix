965bf07bbfc32588f133c33104da2741c6ae228f
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-08 16:41:09 -0400

    Open up firewall ports for kde connect

--- modules/common/messaging/default.nix
+++ modules/common/messaging/default.nix
@@ -8,7 +8,17 @@
   config = lib.mkIf config.mine.messaging.enable {
     # Home manager config
     home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
+
+    networking.firewall = rec {
+      allowedTCPPortRanges = [{
+        from = 1714;
+        to = 1764;
+      }];
+      allowedUDPPortRanges = allowedTCPPortRanges;
+    };
+
   } // (if isDarwin then {
     homebrew.casks = [ "slack" "signal" "caprine" "discord" ];
-  } else { });
+  } else
+    { });
 }
