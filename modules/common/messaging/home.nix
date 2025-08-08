5a96712363fdf7ebb5804a915d6a99ff510b3362
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-08 16:41:09 -0400

    Open up firewall ports for kde connect

--- modules/common/messaging/home.nix
+++ modules/common/messaging/home.nix
@@ -1,8 +1,12 @@
-{ config, pkgs, isDarwin, lib, ... }: {
+{ config, bconfig, pkgs, isDarwin, lib, ... }:
+{
 
-} // (if isDarwin then {
-  # Do nothing, managed through homebrew
-} else {
+} // (if isDarwin then
+  {
+    # Do nothing, managed through homebrew
+  }
+else {
   home.packages = with pkgs; [ slack signal-desktop caprine discord ];
   services.kdeconnect.enable = true;
+
 })
