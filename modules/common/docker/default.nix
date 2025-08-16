aea32e323271c191bba678468f36a4dfa0e21d08
kwkaiser <karl@kwkaiser.io>
2025-08-15 22:14:44 -0400

    cleanup

--- modules/common/docker/default.nix
+++ modules/common/docker/default.nix
@@ -1,11 +1,15 @@
-{ pkgs, config, lib, inputs, ... }: {
+{ pkgs, config, lib, inputs, isDarwin, ... }: {
   options = {
     mine.docker.enable = lib.mkEnableOption "Whether or not to enable Docker";
   };
 
   config = lib.mkIf config.mine.docker.enable {
-    virtualisation.docker = { enable = true; };
-
-    users.users.${config.mine.username}.extraGroups = [ "docker" ];
-  };
+    # Stub
+  }
+  // (if isDarwin then {
+  } else
+    { 
+      virtualisation.docker = { enable = true; };
+      users.users.${config.mine.username}.extraGroups = [ "docker" ];
+    });
 }
