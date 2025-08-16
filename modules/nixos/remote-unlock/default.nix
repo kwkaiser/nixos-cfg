7064cf7447b60242e228be39685a7a96d2a73211
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-15 21:05:55 -0400

    gigajank botslop

--- modules/nixos/remote-unlock/default.nix
+++ modules/nixos/remote-unlock/default.nix
@@ -10,6 +10,21 @@
     boot.initrd.network.ssh.enable = true;
     boot.initrd.network.ssh.authorizedKeys = [ config.mine.primarySshKey ];
     boot.initrd.availableKernelModules = [ "r8169" ];
+
+    # generate ephemeral host keys declaratively
+    boot.initrd.network.ssh.hostKeys = let
+      hostKeyED = pkgs.runCommand "initrd-ssh-host-ed25519" {
+        buildInputs = [ pkgs.openssh ];
+      } ''
+        ssh-keygen -t ed25519 -N "" -f $out
+      '';
+      hostKeyRSA = pkgs.runCommand "initrd-ssh-host-rsa" {
+        buildInputs = [ pkgs.openssh ];
+      } ''
+        ssh-keygen -t rsa -N "" -f $out
+      '';
+
+    in [ hostKeyRSA hostKeyED ];
   };
 }
 
