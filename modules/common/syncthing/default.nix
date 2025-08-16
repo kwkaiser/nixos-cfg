90d29b46905570e77dedef3efa1a769e67cf9f43
kwkaiser <karl@kwkaiser.io>
2025-08-16 00:00:29 -0400

    tweak syncthing

--- modules/common/syncthing/default.nix
+++ modules/common/syncthing/default.nix
@@ -1,10 +1,14 @@
-{ pkgs, config, lib, inputs, ... }: {
+{ pkgs, config, lib, inputs, isDarwin, ... }: {
   options = {
     mine.syncthing.enable =
       lib.mkEnableOption "Whether or not to use syncthing";
   };
 
   config = lib.mkIf config.mine.syncthing.enable {
+
+  } // (if isDarwin then {
+    homebrew.casks = [ "syncthing" ];
+  } else {
     home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
-  };
+  });
 }
