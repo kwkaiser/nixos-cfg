1b831bb0cca547037f841a62f858e510b513b8d0
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-15 23:51:41 -0400

    ssh configs

--- modules/common/ssh/home.nix
+++ modules/common/ssh/home.nix
@@ -8,6 +8,32 @@
         user = "kwkaiser";
         proxyJump = "kwkaiser@box.kwkaiser.io";
         forwardAgent = true;
+        extraConfig = ''
+          StrictHostKeyChecking no
+        '';
+      };
+    };
+    matchBlocks = {
+      "desktop-unlock" = {
+        hostname = "192.168.4.110";
+        user = "root";
+        proxyJump = "kwkaiser@box.kwkaiser.io";
+        forwardAgent = true;
+        RemoteCommand = "cryptsetup-askpass";
+        extraConfig = ''
+          StrictHostKeyChecking no
+        '';
+      };
+    };
+    matchBlocks = {
+      "desktop-wakeup" = {
+        hostname = "box.kwkaiser.io";
+        user = "kwkaiser";
+        forwardAgent = true;
+        RemoteCommand = "wakeonlan 70:85:c2:dc:da:23";
+        extraConfig = ''
+          StrictHostKeyChecking no
+        '';
       };
     };
     extraConfig = ''
