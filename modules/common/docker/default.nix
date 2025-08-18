a1700b0fb059e4c004a00366ed9c3488f16eee70
karl@kwkaiser.io <karl@kwkaiser.io>
2025-08-18 11:20:34 -0400

    docker stuff

--- modules/common/docker/default.nix
+++ modules/common/docker/default.nix
@@ -3,10 +3,10 @@
     mine.docker.enable = lib.mkEnableOption "Whether or not to enable Docker";
   };
 
-  config = lib.mkIf config.mine.docker.enable {
-    # Stub
-  } // (if isDarwin then
-    { }
+  config = lib.mkIf config.mine.docker.enable (if isDarwin then
+    {
+      # Darwin-specific Docker configuration (if any)
+    }
   else {
     virtualisation.docker = { enable = true; };
     users.users.${config.mine.username}.extraGroups = [ "docker" ];
