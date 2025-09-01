ccd082e7b30a7a8cfdf052ed1f70f443aad7fc05
karl@kwkaiser.io <karl@kwkaiser.io>
2025-09-01 13:40:26 -0400

    updates, rm deprecated naming for home manager modules

--- modules/default.nix
+++ modules/default.nix
@@ -21,7 +21,7 @@
   # home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.sharedModules =
-    [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
+    [ inputs.plasma-manager.homeModules.plasma-manager ];
 
   home-manager.users.${config.mine.username} = { imports = [ ./hm.nix ]; };
   home-manager.backupFileExtension = "backup";
