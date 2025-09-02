9030e8f52eb241a45fffd918a3f3742aebe0263a
karl@kwkaiser.io <karl@kwkaiser.io>
2025-09-02 13:50:06 -0400

    reduce gc window

--- modules/nix.nix
+++ modules/nix.nix
@@ -21,7 +21,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 3d";
       persistent = true;
     };
     optimise = {
