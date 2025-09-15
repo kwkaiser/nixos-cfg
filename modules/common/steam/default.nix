8c966ae6c306459e90ef017a7b1630668c54fd08
karl@kwkaiser.io <karl@kwkaiser.io>
2025-09-14 20:24:09 -0400

    trying to get gamescope working

--- modules/common/steam/default.nix
+++ modules/common/steam/default.nix
@@ -3,24 +3,25 @@
     mine.steam.enable = lib.mkEnableOption "Enables steam gaming platform";
   };
 
-  config = lib.mkIf config.mine.steam.enable (
-    if isDarwin then {
-      homebrew.casks = [ "steam" ];
-    } else {
-      # home-manager.users.${config.mine.username} = {
-      #   imports = [ ./home.nix ];
-      # };
+  config = lib.mkIf config.mine.steam.enable (if isDarwin then {
+    homebrew.casks = [ "steam" ];
+  } else {
+    # home-manager.users.${config.mine.username} = {
+    #   imports = [ ./home.nix ];
+    # };
 
-      programs.steam = {
-        enable = true;
-        remotePlay.openFirewall =
-          true; 
-        dedicatedServer.openFirewall =
-          true; 
-        localNetworkGameTransfers.openFirewall =
-          true; 
-      };
-    }
-  );
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession = { enable = true; };
+    };
+  });
 }
 
