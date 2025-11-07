aa172920cc2abceb855528ceaa78f7a60e7177fd
karl@kwkaiser.io <karl@kwkaiser.io>
2025-11-07 11:52:02 -0500

    update flake

--- modules/common/nix/home.nix
+++ modules/common/nix/home.nix
@@ -1,4 +1,11 @@
-{ config, pkgs, bconfig, ... }:
-{
-  #  TODO add stuff here
+{ config, pkgs, bconfig, ... }: {
+  home.packages = [
+    (pkgs.writeShellScriptBin "update" ''
+      cd ~/Documents/nixos-cfg && nix flake update && git commit --all --message "update flake" && git ps
+    '')
+
+    (pkgs.writeShellScriptBin "upgrade" ''
+      cd ~/Documents/nixos-cfg && sudo nixos-rebuild switch --flake .#{{.host}}
+    '')
+  ];
 }
