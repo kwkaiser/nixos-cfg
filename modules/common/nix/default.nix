400b0bf78216e2d64a2b03d72df4f443de535591
kwkaiser <karl@kwkaiser.io>
2025-11-28 19:00:57 -0500

    cleanup

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -1,33 +1,68 @@
-{ config, pkgs, isDarwin, lib, ... }:
+{
+  config,
+  pkgs,
+  isDarwin,
+  lib,
+  ...
+}:
 {
   # Global nixpkgs configuration for all systems
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = _: true;
   environment.systemPackages = with pkgs; [ nixfmt-rfc-style ];
-  home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
+  home-manager.users.${config.mine.username} = {
+    imports = [ ./home.nix ];
+  };
 
-} // (if isDarwin then {
-  nix = {
-    enable = false;
-    distributedBuilds = true;
-    settings = {
-      trusted-users = [ "@admin" "kwkaiser" "root" "karl" ];
-      extra-trusted-users = [ "@admin" "kwkaiser" "root" "karl" ];
-      experimental-features = [ "nix-command" "flakes" ];
-    };
-  };
-} else {
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 3d";
-      persistent = true;
-    };
-    optimise = {
-      automatic = true;
-      persistent = true;
-    };
-    settings = { experimental-features = [ "nix-command" "flakes" ]; };
-  };
-})
+}
+// (
+  if isDarwin then
+    {
+      nix = {
+        enable = false;
+        distributedBuilds = true;
+        # external-builders = [{"systems":["aarch64-linux","x86_64-linux"],"program":"/usr/local/bin/determinate-nixd","args":["builder"]}];
+        settings = {
+          trusted-users = [
+            "@admin"
+            "kwkaiser"
+            "root"
+            "karl"
+          ];
+          extra-trusted-users = [
+            "@admin"
+            "kwkaiser"
+            "root"
+            "karl"
+          ];
+          experimental-features = [
+            "nix-command"
+            "flakes"
+            "external-builders"
+          ];
+          extra-experimental-features = "external-builders";
+        };
+      };
+    }
+  else
+    {
+      nix = {
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 3d";
+          persistent = true;
+        };
+        optimise = {
+          automatic = true;
+          persistent = true;
+        };
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+        };
+      };
+    }
+)
