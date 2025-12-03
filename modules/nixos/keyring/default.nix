8908df28330dba6d2368ed7a2992bc7a0135056b
karl@kwkaiser.io <karl@kwkaiser.io>
2025-12-02 21:20:56 -0500

    Keyring stuff

---
+++ modules/nixos/keyring/default.nix
@@ -0,0 +1,34 @@
+{ pkgs, config, lib, ... }: {
+  options = {
+    mine.keyring.enable =
+      lib.mkEnableOption "Enable standardized Linux keyring (GNOME Keyring)";
+  };
+
+  config = lib.mkIf config.mine.keyring.enable {
+    services.gnome.gnome-keyring.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      gnome-keyring
+      libsecret
+      seahorse
+    ];
+
+    security.pam.services = {
+      login.enableGnomeKeyring = true;
+      greetd.enableGnomeKeyring = lib.mkIf config.services.greetd.enable true;
+    };
+
+    # services.dbus.packages = [ pkgs.gnome-keyring ];
+
+    home-manager.users.${config.mine.username} = {
+      services.gnome-keyring = {
+        enable = true;
+        components = [ "pkcs11" "secrets" "ssh" ];
+      };
+
+      home.sessionVariables = {
+        SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/keyring/ssh";
+      };
+    };
+  };
+}
