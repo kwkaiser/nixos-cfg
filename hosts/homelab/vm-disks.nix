ca5a3de12c4dc4c652af75c3371ecc50863f7c8d
karl@kwkaiser.io <karl@kwkaiser.io>
2025-12-04 23:40:33 -0500

    working disabling modules!

--- hosts/homelab/vm-disks.nix
+++ hosts/homelab/vm-disks.nix
@@ -1,5 +1,5 @@
 # systemd will mount an ext4 filesystem at / and zfs will mount the dataset underneath it
-{ lib, ... }: {
+{ ... }: {
   disko.devices = {
     disk = {
 
@@ -7,7 +7,6 @@
       main = {
         type = "disk";
         device = "/dev/sdc";
-        imageSize = "1G";
         content = {
           type = "gpt";
           partitions = {
@@ -40,7 +39,6 @@
       data = {
         type = "disk";
         device = "/dev/sdf";
-        imageSize = "1G";
         content = {
           type = "gpt";
           partitions = {
@@ -56,95 +54,6 @@
         };
       };
 
-      data1 = {
-        type = "disk";
-        device = "/dev/sda";
-        imageSize = "1G";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "data";
-              };
-            };
-          };
-        };
-      };
-      data2 = {
-        type = "disk";
-        device = "/dev/sdb";
-        imageSize = "1G";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "data";
-              };
-            };
-          };
-        };
-      };
-
-      data3 = {
-        type = "disk";
-        device = "/dev/sdd";
-        imageSize = "1G";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "data";
-              };
-            };
-          };
-        };
-      };
-
-      data4 = {
-        type = "disk";
-        device = "/dev/sde";
-        imageSize = "1G";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "data";
-              };
-            };
-          };
-        };
-      };
-    };
-    zpool = {
-      data = {
-        type = "zpool";
-        mode = "mirror";
-        rootFsOptions = {
-          compression = "zstd";
-          "com.sun:auto-snapshot" = "true";
-        };
-        postCreateHook =
-          "zfs list -t snapshot -H -o name | grep -E '^data@blank$' || zfs snapshot data@blank";
-
-        datasets = {
-          "encrypted" = {
-            type = "zfs_fs";
-            mountpoint = "/data";
-          };
-        };
-      };
     };
   };
 }
