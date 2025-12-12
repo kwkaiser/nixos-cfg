03a878ceebeee73e7d06ae2c9d6c37421c48d695
karl@kwkaiser.io <karl@kwkaiser.io>
2025-12-12 15:40:43 -0500

    more subdomains

--- modules/nixos/sys/default.nix
+++ modules/nixos/sys/default.nix
@@ -7,6 +7,22 @@
   config = lib.mkIf config.mine.homelab-dev.enable {
     networking.extraHosts = ''
       127.0.0.1 kwkaiser-test.io
+      127.0.0.1 auth.kwkaiser-test.io
+      127.0.0.1 browser.kwkaiser-test.io
+      127.0.0.1 food.kwkaiser-test.io
+      127.0.0.1 git.kwkaiser-test.io
+      127.0.0.1 irc.kwkaiser-test.io
+      127.0.0.1 lib.kwkaiser-test.io
+      127.0.0.1 movies.kwkaiser-test.io
+      127.0.0.1 news.kwkaiser-test.io
+      127.0.0.1 photos.kwkaiser-test.io
+      127.0.0.1 scurvy.kwkaiser-test.io
+      127.0.0.1 stats.kwkaiser-test.io
+      127.0.0.1 sync.kwkaiser-test.io
+      127.0.0.1 trackers.kwkaiser-test.io
+      127.0.0.1 tv.kwkaiser-test.io
+      127.0.0.1 users.kwkaiser-test.io
+      127.0.0.1 watch.kwkaiser-test.io
     '';
   };
 }
