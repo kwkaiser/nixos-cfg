2e413f2c31ad61af545e067ecd4257b93448d098
karl@kwkaiser.io <karl@kwkaiser.io>
2025-12-19 22:43:10 -0500

    cleanup vm

--- hosts/homelab/vm.nix
+++ hosts/homelab/vm.nix
@@ -5,35 +5,36 @@
     imports = [ ./vm-disks.nix ];
 
     # Port forwarding for convenience
-    virtualisation.forwardPorts = [
-      {
-        from = "host";
-        host.port = 2222;
-        guest.port = 22;
-      }
-      {
-        from = "host";
-        host.port = 6443;
-        guest.port = 6443;
-      }
-      {
-        from = "host";
-        host.port = 80;
-        guest.port = 30080;
-      }
-      {
-        from = "host";
-        host.port = 443;
-        guest.port = 30443;
-      }
-    ];
-
-    virtualisation.memorySize = 8192;
-    virtualisation.cores = 6;
-    virtualisation.sharedDirectories = {
-      nixos-cfg = {
-        source = "/home/kwkaiser/Documents/nixos-cfg";
-        target = "/home/kwkaiser/Documents/nixos-cfg";
+    virtualisation = {
+      forwardPorts = [
+        {
+          from = "host";
+          host.port = 2222;
+          guest.port = 22;
+        }
+        {
+          from = "host";
+          host.port = 6443;
+          guest.port = 6443;
+        }
+        {
+          from = "host";
+          host.port = 80;
+          guest.port = 30080;
+        }
+        {
+          from = "host";
+          host.port = 443;
+          guest.port = 30443;
+        }
+      ];
+      memorySize = 8192;
+      cores = 6;
+      sharedDirectories = {
+        nixos-cfg = {
+          source = "/home/kwkaiser/Documents/nixos-cfg";
+          target = "/home/kwkaiser/Documents/nixos-cfg";
+        };
       };
     };
 
