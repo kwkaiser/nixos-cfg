76011a24e9d9509a3d3f9abb980be993085e1078
karl@kwkaiser.io <karl@kwkaiser.io>
2026-01-15 13:10:46 -0500

    docker tweaks

--- modules/common/docker/default.nix
+++ modules/common/docker/default.nix
@@ -1,30 +1,19 @@
-{
-  pkgs,
-  config,
-  lib,
-  inputs,
-  isDarwin,
-  ...
-}:
-{
+{ pkgs, config, lib, inputs, isDarwin, ... }: {
   options = {
     mine.docker.enable = lib.mkEnableOption "Whether or not to enable Docker";
   };
 
-  config = lib.mkIf config.mine.docker.enable (
-    {
-      home-manager.users.${config.mine.username} = {
-        imports = [ ./home.nix ];
-      };
-    }
-    // lib.optionalAttrs isDarwin {
-      homebrew.casks = [ "docker" ];
-    }
+  config = lib.mkIf config.mine.docker.enable ({
+    home-manager.users.${config.mine.username} = { imports = [ ./home.nix ]; };
+  } // lib.optionalAttrs isDarwin { homebrew.casks = [ "docker" ]; }
     // lib.optionalAttrs (!isDarwin) {
       virtualisation.docker = {
         enable = true;
+        daemon.settings = {
+          log-driver = "journald";
+          log-opts = { max-file = "3"; };
+        };
       };
       users.users.${config.mine.username}.extraGroups = [ "docker" ];
-    }
-  );
+    });
 }
