7acca9c4dfa82eddc864457723d9b5ad75d79f6e
kwkaiser <karl@kwkaiser.io>
2026-01-20 20:43:15 -0500

    nix search

--- modules/common/nix/home.nix
+++ modules/common/nix/home.nix
@@ -1,14 +1,27 @@
-{ config, pkgs, bconfig, ... }: {
-  home.packages = [
-    (pkgs.writeShellScriptBin "update" ''
+{
+  config,
+  pkgs,
+  bconfig,
+  ...
+}:
+{
+  home.packages = with pkgs; [
+    fzf
+    nix-search-tv
+
+    (writeShellScriptBin "ns" ''
+      nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history
+    '')
+
+    (writeShellScriptBin "update" ''
       cd ~/Documents/nixos-cfg && git pl && nix flake update && git commit --all --message "update flake" && git ps
     '')
 
-    (pkgs.writeShellScriptBin "cleanup" ''
+    (writeShellScriptBin "cleanup" ''
       nix-collect-garbage -d && sudo nix-collect-garbage -d
     '')
 
-    (pkgs.writeShellScriptBin "upgrade" ''
+    (writeShellScriptBin "upgrade" ''
       cd ~/Documents/nixos-cfg
       if [ "$(hostname)" = "karls-MacBook-Pro" ]; then
         sudo nix run nix-darwin -- switch --flake .#work-macbook
