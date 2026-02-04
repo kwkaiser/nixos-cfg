fcc19bc63fe150f8f60869ec614a53f3768ccd9f
kwkaiser <karl@kwkaiser.io>
2026-02-04 13:33:52 -0800

    mapleader

--- modules/common/neovim/home.nix
+++ modules/common/neovim/home.nix
@@ -13,14 +13,12 @@
   # };
   programs.nvf = {
     enable = true;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
     settings = {
       vim.viAlias = false;
       vim.vimAlias = true;
-      vim.lsp = {
-        enable = true;
-      };
+      vim.globals.mapleader = " ";
+      vim.lsp.enable = true;
+      vim.filetree.nvimTree.enable = true;
     };
   };
 }
