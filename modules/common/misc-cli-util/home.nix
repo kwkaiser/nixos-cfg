50dcec0b26822e8efb1d1caddf801c2635153908
karl@kwkaiser.io <karl@kwkaiser.io>
2026-02-11 10:04:27 -0800

    wip

--- modules/common/misc-cli-util/home.nix
+++ modules/common/misc-cli-util/home.nix
@@ -6,5 +6,14 @@
 
   programs.tmux = {
     enable = true;
+    shortcut = "a"; # Ctrl+a as leader key
+    baseIndex = 1; # Windows start at 1
+    keyMode = "vi";
+    customPaneNavigationAndResize = true; # Ctrl+a + hjkl for pane navigation
+
+    extraConfig = ''
+      # Map 0 to window 10
+      bind-key 0 select-window -t :10
+    '';
   };
 }
