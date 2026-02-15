356f0c3bdc47d1860856cfcb703a6a6e22589379
karl@kwkaiser.io <karl@kwkaiser.io>
2026-02-15 10:11:03 -0800

    auto garbage collect

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -9,77 +9,80 @@
   # Global nixpkgs configuration for all systems
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = _: true;
-  environment.systemPackages = with pkgs; [ nixfmt-rfc-style ];
+  environment.systemPackages = with pkgs; [nixfmt-rfc-style];
   home-manager.users.${config.mine.username} = {
-    imports = [ ./home.nix ];
+    imports = [./home.nix];
   };
-
 }
 // (
-  if isDarwin then
-    {
-      nix = {
-        enable = true;
-        linux-builder = {
-          enable = false;
-          systems = [
-            "x86_64-linux"
-            "aarch64-linux"
-          ];
-          ephemeral = true;
-          maxJobs = 6;
-          config = {
-            virtualisation = {
-              cores = 6;
-              darwin-builder = {
-                diskSize = 30 * 1024;
-                memorySize = 6 * 1024;
-              };
+  if isDarwin
+  then {
+    nix = {
+      enable = true;
+      optimise.automatic = true;
+      gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 3d";
+      };
+      linux-builder = {
+        enable = false;
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        ephemeral = true;
+        maxJobs = 6;
+        config = {
+          virtualisation = {
+            cores = 6;
+            darwin-builder = {
+              diskSize = 30 * 1024;
+              memorySize = 6 * 1024;
             };
-            # We have to emulate aarch64 on x86 qemu, see https://github.com/golang/go/issues/69255
-            boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
           };
-        };
-        distributedBuilds = true;
-        settings = {
-          trusted-users = [
-            "@admin"
-            "kwkaiser"
-            "root"
-            "karl"
-          ];
-          extra-trusted-users = [
-            "@admin"
-            "kwkaiser"
-            "root"
-            "karl"
-          ];
-          experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
+          # We have to emulate aarch64 on x86 qemu, see https://github.com/golang/go/issues/69255
+          boot.binfmt.emulatedSystems = ["x86_64-linux"];
         };
       };
-    }
-  else
-    {
-      nix = {
-        gc = {
-          automatic = true;
-          dates = "weekly";
-          options = "--delete-older-than 3d";
-          persistent = true;
-        };
-        optimise = {
-          automatic = true;
-          persistent = true;
-        };
-        settings = {
-          experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
-        };
+      distributedBuilds = true;
+      settings = {
+        trusted-users = [
+          "@admin"
+          "kwkaiser"
+          "root"
+          "karl"
+        ];
+        extra-trusted-users = [
+          "@admin"
+          "kwkaiser"
+          "root"
+          "karl"
+        ];
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
       };
-    }
+    };
+  }
+  else {
+    nix = {
+      gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 3d";
+      };
+      optimise = {
+        automatic = true;
+        persistent = true;
+      };
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+      };
+    };
+  }
 )
--- modules/common/node/home.nix
+++ modules/common/node/home.nix
@@ -6,6 +6,5 @@
     cairo
     pixman
     fontconfig
-    pnpm
   ];
 }
