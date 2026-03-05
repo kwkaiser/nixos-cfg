8b44df71658e8ee50751fcdfce3145efe4957c66
karl@kwkaiser.io <karl@kwkaiser.io>
2026-03-05 11:22:32 -0500

    Add linear on work laptop

--- modules/common/work/default.nix
+++ modules/common/work/default.nix
@@ -1,18 +1,19 @@
 {
-  pkgs,
   config,
   lib,
-  inputs,
+  isDarwin,
   ...
-}:
-{
+}: {
   options = {
     mine.work.enable = lib.mkEnableOption "Grab bag for misc work scripts / setup stuff";
   };
 
-  config = lib.mkIf config.mine.work.enable {
-    home-manager.users.${config.mine.username} = {
-      imports = [ ./home.nix ];
-    };
-  };
+  config = lib.mkIf config.mine.work.enable ({
+      home-manager.users.${config.mine.username} = {
+        imports = [./home.nix];
+      };
+    }
+    // lib.optionalAttrs isDarwin {
+      homebrew.casks = ["linear-linear"];
+    });
 }
