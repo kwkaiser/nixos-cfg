4c055a3707ad7a696f61d5902da8e51a6f309a8a
karl@kwkaiser.io <karl@kwkaiser.io>
2026-03-23 20:31:16 -0400

    wip

--- modules/common/misc-cli-util/home.nix
+++ modules/common/misc-cli-util/home.nix
@@ -1,4 +1,7 @@
-{...}: {
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    whisky
+  ];
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
--- modules/common/steam/default.nix
+++ modules/common/steam/default.nix
@@ -1,31 +1,28 @@
 {
-  pkgs,
   lib,
   config,
   isDarwin,
   ...
-}:
-{
+}: {
   options = {
     mine.steam.enable = lib.mkEnableOption "Enables steam gaming platform";
   };
 
   config = lib.mkIf config.mine.steam.enable (
-    if isDarwin then
-      {
-        homebrew.casks = [ "steam" ];
-      }
-    else
-      {
-        programs.steam = {
+    if isDarwin
+    then {
+      homebrew.casks = ["steam"];
+    }
+    else {
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+        gamescopeSession = {
           enable = true;
-          remotePlay.openFirewall = true;
-          dedicatedServer.openFirewall = true;
-          localNetworkGameTransfers.openFirewall = true;
-          gamescopeSession = {
-            enable = true;
-          };
         };
-      }
+      };
+    }
   );
 }
