92ec5643234b85464a9338002c383ca356143f98
karl@kwkaiser.io <karl@kwkaiser.io>
2026-03-31 11:50:33 -0400

    worktrunk

--- modules/common/misc-cli-util/default.nix
+++ modules/common/misc-cli-util/default.nix
@@ -3,6 +3,7 @@
   config,
   lib,
   inputs,
+  isDarwin,
   ...
 }:
 {
@@ -10,9 +11,12 @@
     mine.misc-cli-util.enable = lib.mkEnableOption "Whether or not to enable misc cli util scripts";
   };
 
-  config = lib.mkIf config.mine.misc-cli-util.enable {
+  config = lib.mkIf config.mine.misc-cli-util.enable ({
     home-manager.users.${config.mine.username} = {
       imports = [ ./home.nix ];
     };
-  };
+  } // lib.optionalAttrs isDarwin {
+    homebrew.taps = [ "max-sixty/tap" ];
+    homebrew.brews = [ "max-sixty/tap/worktrunk" ];
+  });
 }
