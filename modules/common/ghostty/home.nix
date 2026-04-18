2de0589042c2c39be16b2be3a01d08dc08f3f278
karl@kwkaiser.io <karl@kwkaiser.io>
2026-04-17 22:19:26 -0700

    wip

--- modules/common/ghostty/home.nix
+++ modules/common/ghostty/home.nix
@@ -1,7 +1,8 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   programs = {
     ghostty = {
       enable = true;
+      package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
       settings = {
         confirm-close-surface = false;
       };
