0d124ccf38680aac779dd0505fb11720673d8f01
karl@kwkaiser.io <karl@kwkaiser.io>
2026-04-28 16:35:03 -0700

    wip

--- modules/hm.nix
+++ modules/hm.nix
@@ -1,6 +1,15 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, isDarwin, ... }:
 
 {
   home.stateVersion = "24.11";
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = lib.optionals isDarwin [
+    (final: prev: {
+      # TODO: remove — direnv zsh tests hang in macOS sandbox
+      direnv = prev.direnv.overrideAttrs (old: {
+        doCheck = false;
+        checkPhase = "";
+      });
+    })
+  ];
 }
