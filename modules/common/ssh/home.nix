f90d9a57396b46af4926ecebc87235163a935e28
karl@kwkaiser.io <karl@kwkaiser.io>
2026-07-15 16:57:33 -0400

    mac stuff

--- modules/common/ssh/home.nix
+++ modules/common/ssh/home.nix
@@ -22,6 +22,7 @@
         ServerAliveInterval = 300;
         ServerAliveCountMax = 2;
         TCPKeepAlive = "yes";
+        MACs = "hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-sha1";
       };
       "homelab-vm" = {
         Hostname = "localhost";
