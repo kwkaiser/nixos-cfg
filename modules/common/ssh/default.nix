9e1ba45eda59709942de23ba8fff92eb8c3646dd
karl@kwkaiser.io <karl@kwkaiser.io>
2026-07-18 21:00:33 -0400

    tweak

--- modules/common/ssh/default.nix
+++ modules/common/ssh/default.nix
@@ -12,35 +12,36 @@
     };
   };
 
-  config = lib.mkIf config.mine.ssh.enable {
-    mine.ssh.server.enable = lib.mkDefault true;
+  config = lib.mkIf config.mine.ssh.enable ({
+      mine.ssh.server.enable = lib.mkDefault true;
 
-    services.openssh.enable = config.mine.ssh.server.enable;
+      services.openssh.enable = config.mine.ssh.server.enable;
 
-    programs.ssh =
-      {
-      }
-      // lib.optionalAttrs (!isDarwin) {
-        startAgent = true;
-      };
-
-    home-manager.users.${config.mine.username} = {imports = [./home.nix];};
-
-    launchd.user.agents.ssh-config-guard = lib.mkIf isDarwin {
-      serviceConfig = {
-        ProgramArguments = [
-          "${config.home-manager.users.${config.mine.username}.home.activationPackage}/activate"
-        ];
-        EnvironmentVariables = {
-          HOME = config.mine.homeDir;
-          USER = config.mine.username;
-          LOGNAME = config.mine.username;
+      programs.ssh =
+        {
+        }
+        // lib.optionalAttrs (!isDarwin) {
+          startAgent = true;
         };
-        WatchPaths = ["${config.mine.homeDir}/.ssh/config"];
-        RunAtLoad = true;
-        StandardOutPath = "${config.mine.homeDir}/Library/Logs/ssh-config-guard.log";
-        StandardErrorPath = "${config.mine.homeDir}/Library/Logs/ssh-config-guard.log";
+
+      home-manager.users.${config.mine.username} = {imports = [./home.nix];};
+    }
+    // lib.optionalAttrs isDarwin {
+      launchd.user.agents.ssh-config-guard = {
+        serviceConfig = {
+          ProgramArguments = [
+            "${config.home-manager.users.${config.mine.username}.home.activationPackage}/activate"
+          ];
+          EnvironmentVariables = {
+            HOME = config.mine.homeDir;
+            USER = config.mine.username;
+            LOGNAME = config.mine.username;
+          };
+          WatchPaths = ["${config.mine.homeDir}/.ssh/config"];
+          RunAtLoad = true;
+          StandardOutPath = "${config.mine.homeDir}/Library/Logs/ssh-config-guard.log";
+          StandardErrorPath = "${config.mine.homeDir}/Library/Logs/ssh-config-guard.log";
+        };
       };
-    };
-  };
+    });
 }
