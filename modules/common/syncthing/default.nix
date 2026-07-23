c34fed28522e25157974000b3a565d2dc4d9cccb
karl@kwkaiser.io <karl@kwkaiser.io>
2026-07-23 15:01:27 -0400

    syncthing

--- modules/common/syncthing/default.nix
+++ modules/common/syncthing/default.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  isDarwin,
   ...
 }:
 {
@@ -10,17 +9,9 @@
     mine.syncthing.enable = lib.mkEnableOption "Enables syncthing";
   };
 
-  config = lib.mkIf config.mine.syncthing.enable (
-    {
-
-    }
-    // lib.optionalAttrs isDarwin {
-      homebrew.brews = [ "syncthing" ];
-    }
-    // lib.optionalAttrs (!isDarwin) {
-      home-manager.users.${config.mine.username} = {
-        imports = [ ./home.nix ];
-      };
-    }
-  );
+  config = lib.mkIf config.mine.syncthing.enable {
+    home-manager.users.${config.mine.username} = {
+      imports = [ ./home.nix ];
+    };
+  };
 }
