0ac47ccfa9d47fd3957b6cf098384c6cf036e377
karl@kwkaiser.io <karl@kwkaiser.io>
2026-07-28 19:59:55 -0400

    update syncthing id

--- modules/common/syncthing/home.nix
+++ modules/common/syncthing/home.nix
@@ -1,6 +1,9 @@
-{ selfDevice }:
-{ config, pkgs, lib, ... }:
-let
+{selfDevice}: {
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   ignorePatterns = [
     ".DS_Store"
     "._*"
@@ -12,28 +15,27 @@ let
 
   deviceIds = {
     phone = "C2OL7VB-VVCL6CM-2ZLO7N4-RLTP7GO-EN3EZSD-QPZL3XC-VTI7IUC-BDDCEAV";
-    desktop = "EN3PXUV-4CGWE5S-HJHI7ZE-BC2CH2X-TDV4SGQ-NL7XHRR-KBAEJEJ-74GQNQR";
+    desktop = "GUNDSXG-DIIM4AU-IF2MLGU-TAGH3RP-X7AJELO-VJAEZAF-4VMV4QI-SQIT2AQ";
     server = "KCLNUZ7-P2YEIO4-WNZ7O6L-TXWD3VI-TETMH45-GQJKLNC-LMEFDIV-B7XBFAM";
     pallet-macbook = "6JZGMBT-TX43LZJ-L7VCKGI-ZTSAJNV-GFAPC66-ENZ5UDE-SGT2XQV-I3RWFA7";
   };
 
-  allDevices = lib.mapAttrs (name: id: { inherit id; }) deviceIds;
+  allDevices = lib.mapAttrs (name: id: {inherit id;}) deviceIds;
 
   # Who each device is allowed to connect to. pallet-macbook (work laptop,
   # employer-controlled) only talks to server, so cutting it off is a single
   # edit here rather than something enforced per-device.
   topology = {
-    phone = [ "desktop" "server" ];
-    desktop = [ "phone" "server" ];
-    server = [ "phone" "desktop" "pallet-macbook" ];
-    pallet-macbook = [ "server" ];
+    phone = ["desktop" "server"];
+    desktop = ["phone" "server"];
+    server = ["phone" "desktop" "pallet-macbook"];
+    pallet-macbook = ["server"];
   };
 
-  allowedPeers = topology.${selfDevice} or [ ];
+  allowedPeers = topology.${selfDevice} or [];
 
   folderDevices = builtins.filter (d: builtins.elem d allowedPeers) (builtins.attrNames allDevices);
-in
-{
+in {
   services.syncthing = {
     enable = true;
     settings = {
