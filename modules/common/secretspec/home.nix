63210d0bd37ab816585e47e48c68c4b65b87e014
karl@kwkaiser.io <karl@kwkaiser.io>
2026-08-07 14:29:17 -0400

    ss

--- modules/common/secretspec/home.nix
+++ modules/common/secretspec/home.nix
@@ -1,7 +1,19 @@
 { pkgs, bconfig, ... }: {
-  home.packages = [ pkgs.secretspec ];
+  home.packages = [
+    pkgs.secretspec
+    (pkgs.writeShellScriptBin "ensure-kdbx-password" ''
+      if [ -z "$SECRETSPEC_KDBX_PASSWORD" ]; then
+        read -rsp "kdbx password: " SECRETSPEC_KDBX_PASSWORD
+        echo >&2
+        export SECRETSPEC_KDBX_PASSWORD
+      fi
+    '')
+  ];
 
   xdg.configFile."secretspec/config.toml".text = ''
+    [defaults]
+    provider = "kdbx"
+
     [defaults.providers.kdbx]
     uri = "kdbx:${bconfig.mine.homeDir}/Documents/keys/keys.kdbx"
   '';
