969d0e56d7f7dad070067fe4a42fdc0b57dab367
karl@kwkaiser.io <karl@kwkaiser.io>
2026-09-08 19:00:15 -0400

    rosetta

--- modules/sikarugir.nix
+++ modules/sikarugir.nix
@@ -5,5 +5,11 @@
   config.darwin.modules.sikarugir = {
     homebrew.taps = [ "Sikarugir-App/sikarugir" ];
     homebrew.casks = [ "Sikarugir-App/sikarugir/sikarugir" ];
+
+    system.activationScripts.postActivation.text = ''
+      if [ "$(uname -p)" = "arm" ] && ! /usr/bin/arch -x86_64 /usr/bin/true >/dev/null 2>&1; then
+        softwareupdate --install-rosetta --agree-to-license
+      fi
+    '';
   };
 }
